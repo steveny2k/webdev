b3cf36f63daac4220efe6d7dd336513d6615ec0c
Ryan Morris <mr.morris@gmail.com>
2019-02-03 12:26:21 -0500

    Notes on partials

--- src/www/js/partial/partial.js
+++ src/www/js/partial/partial.js
@@ -1,4 +1,16 @@
-Function.prototype.curry = function() {
+/**
+ * Extends Functions so that they have a method, curry
+ *  function add (x, y, z) {}
+ *  add.curry()
+ * This function acts like a partial "maker", it returns a new function
+ *  partialMaker = add.curry()
+ * The new function will curry arguments onto the original function
+ * The maker function returns a third function which represents the original
+ * "curried" with arguments
+ *  add10 = partialMaker(10);
+ *  add10(4); // add(10, 4)
+ */
+Function.prototype.curry = function () {
 
   // Your code here.
 
