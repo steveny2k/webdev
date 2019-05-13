92ab79110c53b08cd3657306c29efdfa626f6bed
Ryan Morris <mr.morris@gmail.com>
2019-05-13 18:44:48 -0400

    Add combo hosts solution

--- src/www/js/hosts/hosts.js
+++ src/www/js/hosts/hosts.js
@@ -48,32 +48,83 @@
  * Make sure your tests still pass.
  */
 Hosts = (function () {
-	let hosts = {};
+  let hosts = {};
 
-	return {
-		add(name, address) {
-			if (!hosts.hasOwnProperty(name)) {
-				hosts[name] = [];
-			}
-			hosts[name].push(address);
-		},
-		lookupByName(name) {
-			if (hosts.hasOwnProperty(name)) {
-				return hosts[name];
-			}
-			return [];
-		},
-		lookupByIP(address) {
-			let matches = [];
-			Object.getOwnPropertyNames(hosts).forEach((name) => {
-				if (hosts[name].includes(address)) {
-					matches.push(name);
-				}
-			});
-			return matches;
-		},
-		clear() {
-			hosts = {};
-		}
-	}
+  return {
+    add(name, address) {
+      if (!hosts.hasOwnProperty(name)) {
+        hosts[name] = [];
+      }
+      hosts[name].push(address);
+    },
+    lookupByName(name) {
+      if (hosts.hasOwnProperty(name)) {
+        return hosts[name];
+      }
+      return [];
+    },
+    lookupByIP(address) {
+      let matches = [];
+      Object.getOwnPropertyNames(hosts).forEach((name) => {
+        if (hosts[name].includes(address)) {
+          matches.push(name);
+        }
+      });
+      return matches;
+    },
+    clear() {
+      hosts = {};
+    }
+  }
 })();
+
+/*
+Hosts = (function () {
+
+  const hostMap = new Map();
+
+  // private stuff...
+  const add = function (host, ip) {
+    let ips;
+    if (hostMap.has(host)) {
+      ips = hostMap.get(host);
+    } else {
+      ips = [];
+    }
+    ips.push(ip);
+    //hostMap.set(host, ips);
+  }
+
+  const lookupByName = function (host) {
+    if (hostMap.has(host)) {
+      return hostMap.get(host);
+    }
+    return [];
+  }
+
+  const lookupByIP = function (ip) {
+    let foundHosts = [];
+
+    hostMap.forEach(function (ips, host) {
+      if (ips.includes(ip)) {
+        foundHosts.push(host);
+      }
+    });
+    return foundHosts;
+  }
+
+  const clear = function () {
+    hostMap.clear();
+  }
+
+  // public stuff
+  return {
+    add,
+    lookupByName,
+    lookupByIP,
+    clear,
+  }
+
+})();
+/**/
+
